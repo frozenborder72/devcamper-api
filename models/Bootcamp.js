2f0a76ababedc2fdefb93ecad625957467a19a1c
frozenborder72 <mar.manunta@gmail.com>
2024-03-10 19:05:41 +0100

    create bootcamp - POST

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -98,4 +98,4 @@ const BootcampSchema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.Model('Bootcamp', BootcampSchema);
+module.exports = mongoose.model('Bootcamp', BootcampSchema);
